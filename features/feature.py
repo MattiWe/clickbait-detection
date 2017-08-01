7870d62ea92c371a30047b8361f29f7f05c89981
Matti Wiegmann <matti.wiegmann@uni-weimar.de>
2017-08-01 15:48:04 +0200

    started integrating learning

--- features/feature.py
+++ features/feature.py
@@ -2,6 +2,7 @@
 import numpy as np
 import json
 from itertools import combinations
+from collections import deque
 
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import word_tokenize
@@ -55,16 +56,19 @@ class ClickbaitDataset(object):
 
         self.dataset_dict = {}
         for i in _instances:
-            self.dataset_dict[i['id']] = {'postText': i['postText'], 'targetTitle': i['targetTitle'],
-                                          'targetDescription': i['targetDescription'], 'targetKeywords': i['targetKeywords'],
-                                          'targetParagraphs': i['targetParagraphs'], 'targetCaptions': i['targetCaptions']}
+            self.dataset_dict[i['id']] = {'postText': i['postText'],
+                                          'targetTitle': i['targetTitle'],
+                                          'targetDescription': i['targetDescription'],
+                                          'targetKeywords': i['targetKeywords'],
+                                          'targetParagraphs': i['targetParagraphs'],
+                                          'targetCaptions': i['targetCaptions']}
         for t in _truth:
             self.dataset_dict[t['id']]['truthMean'] = t['truthMean']
 
         self.id_index = {index: key for index, key in enumerate(self.dataset_dict.keys())}
 
     def get_y(self):
-        return [self.dataset_dict[self.id_index[key]]['truthMean'] for key in sorted(self.id_index.keys())]
+        return np.asarray([self.dataset_dict[self.id_index[key]]['truthMean'] for key in sorted(self.id_index.keys())])
 
     # TODO generic for all columns
     def get_x_posttext(self):
@@ -98,7 +102,7 @@ class NGramFeature(Feature):
         self.feature = self.vectorizer.fit_transform(self.data.get_x_posttext())
 
     def assparse(self):
-        return self.feature
+        return scipy.sparse.csc_matrix(self.feature)
 
 
 class SingleVectorFeature(Feature):
@@ -119,17 +123,25 @@ class FeatureBuilder(object):
         return self
 
     def build(self, _features=None, split=False):
-        _result = scipy.sparse.csc_matrix((self.data.size(), 1))
+        _result = None
+
+        def push(result, f):
+            if result is None:
+                result = f
+            else:
+                result = scipy.sparse.hstack((_result, f))
+            return result
+
         __features = self.features.values() if _features is None else _features
         for f in __features:
             if isinstance(f, Feature):
-                _result = scipy.sparse.hstack((_result, f.assparse()))
+                _result = push(_result, f.assparse())
             elif isinstance(f, scipy.sparse.spmatrix):
-                _result = scipy.sparse.hstack((_result, f))
+                _result = push(_result, f)
             elif isinstance(f, np.array):
-                _result = scipy.sparse.hstack((_result, f[:, np.newaxis]))
+                _result = push(_result, f[:, np.newaxis])
             elif isinstance(f, np.ndarray):
-                _result = scipy.sparse.hstack((_result, scipy.sparse.csc_matrix(f)))
+                _result = push(_result, scipy.sparse.csc_matrix(f))
 
         if split:
             return train_test_split(_result, np.asarray(self.data.get_y()).T, random_state=42)
@@ -145,30 +157,49 @@ class FeatureBuilder(object):
     def get_combinations(self, m=1, n=1):
         '''Returns every n-o column combination of the build features as a list.
             E.g. for 1-2: every column and every 2-combination of columns'''
-        list_of_features = [scipy.sparse.csc_matrix(x_train[:, x]) for x in range(x_train.shape[1])]
-        _result = []
+        # learn
+        def learn(x, y):
+            x_train, x_test, y_train, y_test = train_test_split(x, y.T, random_state=42)
+            model = svm.SVR()
+            model.fit(x_train, y_train)
+            y_predicted = model.predict(x_test)
+            return mean_squared_error(y_test, y_predicted)
+
+        _x, _y = self.build()
+        list_of_features = [scipy.sparse.csc_matrix(_x[:, x]) for x in range(_x.shape[1])]
+
+        list_of_mse = [learn(x, _y) for x in list_of_features]
+
+        print(list_of_mse)
+        '''_result = deque()
         for i in range(m, n + 1):
-            comb = combinations(list_of_features, i)
+            print(i)
+            # get combinations of indices
+            comb = combinations(range(len(list_of_features)), i)
             for j in comb:
-                x = j[0]
-                if len(j > 1):
-                    for k in range(1, len(j)):
-                        j = scipy.sparse.hstack((x, j[k]))
+                tup = [list_of_features[t] for t in j]
+                x = scipy.sparse.hstack(tup)
                 _result.append(x)
-        return _result
+        return list(_result)'''
 
 
 if __name__ == "__main__":
     # get list of scores and a list of the postTexts
     cbd = ClickbaitDataset("../clickbait17-train-170331/instances.jsonl", "../clickbait17-train-170331/truth.jsonl")
     f = NGramFeature(data=cbd, vectorizer=CountVectorizer, n=1)
+    print(f.assparse().shape)
 
-    x_train, x_test, y_train, y_test = FeatureBuilder(cbd).add_feature("charonegramcount", f) \
+    '''x_train, x_test, y_train, y_test = FeatureBuilder(cbd).add_feature("charonegramcount", f) \
                                                           .add_feature('test', f.assparse())  \
-                                                          .compose_split('test')
+                                                          .build(split=True)
+    '''
+    x, y = FeatureBuilder(cbd).add_feature("charonegramcount", f) \
+                              .build()
+    print(x.shape)
+    feature_combinations = FeatureBuilder(cbd).add_feature("charonegramcount", f) \
+                                              .get_combinations(1, 2)
 
-    print(x_train.shape)
-    model = svm.SVR()
-    model.fit(x_train, y_train)
-    y_predicted = model.predict(x_test)
-    print(mean_squared_error(y_test, y_predicted))
+    # print(x_train.shape)
+    print(len(feature_combinations))
+    '''
+    '''
