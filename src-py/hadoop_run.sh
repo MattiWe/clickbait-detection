1740265df4fa588953a485669d5400146b0ad634
Matti Wiegmann <matti.wiegmann@uni-weimar.de>
2017-08-23 13:33:00 +0200

    moved

--- src-py/hadoop_run.sh
+++ src-py/hadoop_run.sh
@@ -8,8 +8,10 @@ reducer="hadoop_reducer.py"
 # 1. copy files to hadoop fs
 hadoop fs put $mapper $mapper
 hadoop fs put $reducer $reducer
+# TODO put npzs on hdfs
+# put module on hdfs
 
-# 2. run mapreduce
+# 2. run mapreduce # TODO append ml module
 hadoop jar "$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar" \
        -file $mapper -mapper $mapper \
        -file $reducer -reducer $reducer \
@@ -19,5 +21,3 @@ hadoop jar "$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar" \
 
 # 3. fetch results and update input files
 hadoop fs -cat $out_directory/* # TODO
-
-# 4. goto 2 x times
